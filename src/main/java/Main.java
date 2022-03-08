d05226b42aa6dd77c2d6021f093ef9c8f17b0acf
David Hofmann <83290245+davidhawes@users.noreply.github.com>
2022-03-09 00:01:14 +0100

    erste Iteration eines UserInterface in der CommandLine

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import dataaccess.MysqlDatabaseConnection;
+import ui.Cli;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -6,6 +7,9 @@ import java.sql.SQLException;
 public class Main {
 
     public static void main(String[] args) {
+
+        Cli myClie = new Cli();
+        myClie.start();
 
         try {
             Connection mysqlDatabaseConnection = MysqlDatabaseConnection.getConnection("jdbc:mysql://localhost:3306/kurssystem", "root", "");
