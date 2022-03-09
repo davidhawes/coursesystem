05a328cf1d20f0096a597fed1521ff036f0347ad
David Hofmann <83290245+davidhawes@users.noreply.github.com>
2022-03-09 02:29:37 +0100

    Implementation der getAll Methode; Entkopplung Repo cli/main; showAllCourses() commandline implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,23 @@
-import dataaccess.MysqlDatabaseConnection;
+import dataaccess.MySqlCourseRepository;
 import ui.Cli;
 
-import java.sql.Connection;
 import java.sql.SQLException;
+
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Cli myClie = new Cli();
-        myClie.start();
-
+        Cli myCli = null;
         try {
-            Connection mysqlDatabaseConnection = MysqlDatabaseConnection.getConnection("jdbc:mysql://localhost:3306/kurssystem", "root", "");
-            System.out.println("Verbindung aufgebaut!");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+            myCli = new Cli(new MySqlCourseRepository());
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.out.println("Datenbankfehler: " + e.getMessage() + " SQL State: " + e.getSQLState());
+        } catch (ClassNotFoundException e) {
+            System.out.println("Datenbankfehler: " + e.getMessage());
         }
+        myCli.start();
+
+
     }
 }
